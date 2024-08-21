7ddce29901ea09714cedca7f87eeebc47e36daef
Sam Sabat <samnsabat@gmail.com>
2024-08-21 15:56:45 +0200

    hot fix

--- src/components/listScreen/ListScreen.jsx
+++ src/components/listScreen/ListScreen.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import PropTypes from "prop-types";
+
 function ListScreen({ apiUrl, title, Card }) {
 	const [data, setData] = useState([]);
 	const [loading, setLoading] = useState(true);
